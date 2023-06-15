bcee1617747a96596529e403aea7f7d298d95f23
Junior-Shyko <franciscaoanto@gmail.com>
2023-06-15 17:41:39 -0300

    Refatorado com remoção de codigo e alterando isLastPhase

--- Plugin.php
+++ Plugin.php
@@ -4,183 +4,157 @@ namespace PrestacaoDeContas;
 
 use MapasCulturais\App;
 use MapasCulturais\Definitions;
+use \PrestacaoDeContas\Repositories\PrestacaoDeContasRepository;
 
 class Plugin extends \MapasCulturais\Plugin
 {
-    public function _init()
-    {
-        $app = App::i();
-        $app->hook('template(opportunity.<<single|edit>>.tab-about--highlighted-message):end', function () use ($app) {
+   public function _init()
+   {
+      $app = App::i();
+      //Edição da Oportunidade
+      $app->hook('template(opportunity.edit.tab-about):begin', function () use ($app, &$valueIsLastPhase) {
+         $countIsPhases = 0; //Total de Oportunidades que são PC
+         $count_total_pc = 0; // total registrado na configuração da oportunidade
 
-            $valueIsLastPhase = 0;
-            $valueCount = 0;
+         /*   $count_Antigo =  $count_total_pc; */
+         if (!is_null($this->data['entity']->parent)) {
 
-            if (!is_null($this->data['entity']->parent)) {
-                if (($this->data['entity']->parent->id)) {
+            if (($this->data['entity']->parent->id)) {
+               /**
+                * Explicar o porque dessa consulta
+                */
+               //   $parent = $app->repo('OpportunityMeta')->findBy([
+               //       'owner' => $this->data['entity']->parent->id,
+               //   ]);
 
-                    $parent = $app->repo('OpportunityMeta')->findBy([
-                        'owner' => $this->data['entity']->parent->id,
-                    ]);
+               //Quantidade de prestações de conta
+               $idsFilhos = [];
+               $opp = $app->repo('Opportunity')->findBy([
+                  'parent' => $this->data['entity']->parent->id,
+               ]);
+         
+               foreach ($opp as $key => $value) {
+                  array_push($idsFilhos, $value->id);
+               }
 
-                    //recebe o valor do isLastPhase
-                    foreach ($parent as $itensOpp) {
-                        if ($itensOpp->key == 'isLastPhase') {
-                            $valueIsLastPhase = $itensOpp->value;
-                        }
-                    }
+               sort($idsFilhos);
 
-                    //pegar valor do count
-                    foreach ($parent as $itensOpp) {
-                        if ($itensOpp->key == 'count_total_pc') {
-                            $valueCount = $itensOpp->value;
-                        }
-                    }
-                }
-            } // fim if PAI
+               //contar as fases de prestação de contas
+               foreach ($idsFilhos as $valChild) {
+                  $child = $app->repo('OpportunityMeta')->findBy([
+                     'owner' => $valChild,
+                  ]);
 
-            $app->view->part('widget-opportunity-phases2', ['valueIsLastPhase' => $valueIsLastPhase]);
-        });
+                  foreach ($child as $ChildrenValue) {
+                     if ($ChildrenValue->key == 'isOpportunityPhase') {
+                        $countIsPhases++;
+                     }
+                  }
+               }
 
-        //Edição da Oportunidade
-        $app->hook('template(opportunity.edit.tab-about):begin', function () use ($app, &$valueIsLastPhase) {
-            $countIsPhases = 0; //Total de Oportunidades que são PC
-            $count_total_pc = 0; // total registrado na configuração da oportunidade
+               //pega o valor total de fases
+               foreach ($idsFilhos as $key => $valChild) {
+                  $parent = $app->repo('OpportunityMeta')->findBy([
+                     'owner' => $this->data['entity']->parent->id,
+                  ]);
 
-            $oldValue = 0; //
-
-            /*   $count_Antigo =  $count_total_pc; */
-            if (!is_null($this->data['entity']->parent)) {
-
-                if (($this->data['entity']->parent->id)) {
-                    $parent = $app->repo('OpportunityMeta')->findBy([
-                        'owner' => $this->data['entity']->parent->id,
-                    ]);
-
-                    //Quantidade de prestações de conta
-                    $idsFilhos = [];
-                    $opp = $app->repo('Opportunity')->findBy([
-                        'parent' => $this->data['entity']->parent->id,
-                    ]);
-
-                    foreach ($opp as $key => $value) {
-                        array_push($idsFilhos, $value->id);
-                    }
-
-                    sort($idsFilhos);
-
-                    //contar as fases de prestação de contas
-                    foreach ($idsFilhos as $key => $valChild) {
-                        // dump($valChild);
-                        $child = $app->repo('OpportunityMeta')->findBy([
-                            'owner' => $valChild,
-                        ]);
-
-                        foreach ($child as $ChildrenValue) {
-                            if ($ChildrenValue->key == 'isOpportunityPhase') {
-                                $countIsPhases++;
-                            }
-                        }
-                    }
-
-                    //pega o valor total de fases
-                    foreach ($idsFilhos as $key => $valChild) {
-                        $parent = $app->repo('OpportunityMeta')->findBy([
-                            'owner' => $this->data['entity']->parent->id,
-                        ]);
-
-                        foreach ($parent as $itensOpp) {
-                            if ($itensOpp->key == 'count_total_pc') {
-                                $count_total_pc = $itensOpp->value;
-                            }
-                        }
-                    }
-                }
+                  foreach ($parent as $itensOpp) {
+                     if ($itensOpp->key == 'count_total_pc') {
+                        $count_total_pc = $itensOpp->value;
+                     }
+                  }
+               }
             }
+         }
 
-            $entity = $app->view->controller->requestedEntity;
-            $app->view->enqueueScript('app', 'prestacaodecontas', 'js/prestacaodecontas/prestacaodecontas.js');
-            $app->view->part('widget-accountability-phases', ['entity' => $entity]);
+         $entity = $app->view->controller->requestedEntity;
+         $app->view->enqueueScript('app', 'prestacaodecontas', 'js/prestacaodecontas/prestacaodecontas.js');
+         $app->view->part('widget-accountability-phases', ['entity' => $entity]);
 
-            self::upLastPhase($app, $countIsPhases, $count_total_pc, $this->data['entity']->parent, 0);
-        });
+         self::upLastPhase($app, $countIsPhases, $count_total_pc, $this->data['entity']->parent, 0);
+      });
 
-        //HOOK para ficar escutando a mudança de valores do select
-
-        $app->hook("entity(Opportunity).update:after", function () use ($app) {
+      //HOOK para realizar alteração apos o salvar
+      $app->hook("entity(Opportunity).update:after", function () use ($app) {
          $entity = $this;
          self::comparacao($entity, $app);
-        });
+      });
+   }
 
-    }
-    static function comparacao($entity, $app)
-    {      
-       $meta = $entity->getMetadata();
-       dump($meta['count_total_pc']); 
-       $opp = $app->repo('Opportunity')->findBy([
-          'parent' => $entity->id,
-       ]);
-       dump(count($opp));
- 
-       if( (int) $meta['count_total_pc'] <> count($opp))
-       {
-          self::upLastPhase($app, null, $meta['count_total_pc'], $entity, 0);
-       }
-    }
-    
-    /**
-     * Altera o valor do isLastPhase para criação de uma nova fase
-     *
-     * @param [Object] $app
-     * @param [integer] $countIsPhases
-     * @param [integer] $count_total_pc
-     * @param [Object] $parent
-     * @return void
-     */
-    protected static function upLastPhase($app, $countIsPhases, $count_total_pc, $parent, $valueUpdate)
-    {
-        $parentId = null;
-        if ($countIsPhases < $count_total_pc) {
+   /**
+    * Realiza uma comparação com o count_total_pc e o total de filhos
+    * Caso count_total_pc seja maior que o total de filhos, então altera o isLastPhase para 1
+    *
+    * @param [object] $entity
+    * @param [object] $app
+    */
+   static function comparacao($entity, $app)
+   {
+      $meta = $entity->getMetadata();
 
-            //pegar id do pai
-            if (!is_null($parent)) {
-                $parentId = $parent->id;
-            }
+      $opp = $app->repo('Opportunity')->findBy([
+         'parent' => $entity->id,
+      ]);
 
-            $phase_up = $app->repo('OpportunityMeta')->findOneBy(['owner' => $parentId, 'key' => 'isLastPhase']);
+      if ((int) $meta['count_total_pc'] > count($opp)) {
+         self::upLastPhase($app, null, $meta['count_total_pc'], $entity, 0);
+      } elseif ((int) $meta['count_total_pc'] <= count($opp)) {
+         self::upLastPhase($app, null, $meta['count_total_pc'], $entity, 1);
+      }
+   }
+
+   /**
+    * Altera o valor do isLastPhase para 0 pu 1 dependendo da necessidade
+    *
+    * @param [Object] $app
+    * @param [integer] $countIsPhases
+    * @param [integer] $count_total_pc
+    * @param [Object] $parent
+    * @param [integer] $valueUpdate
+    * @return void
+    */
+   protected static function upLastPhase($app, $countIsPhases = null, $count_total_pc, $parent, $valueUpdate)
+   {
+
+      if ($countIsPhases < $count_total_pc) {
+         //pegar id do pai
+         if (!is_null($parent)) {
+            $phase_up = $app->repo('OpportunityMeta')->findOneBy(['owner' => $parent->id, 'key' => 'isLastPhase']);
             if ($phase_up) {
-                $phase_up->setValue($valueUpdate);
-                $app->em->persist($phase_up);
-                $app->em->flush();
+               $phase_up->setValue($valueUpdate);
+               $app->em->persist($phase_up);
+               $app->em->flush();
             }
-        }
-    }
+         }
+      }
+   }
 
-    public function register()
-    {
-        $app = App::i();
-        $conf =
-            [
+   public function register()
+   {
+      $app = App::i();
+      $conf =
+         [
             'label' => \MapasCulturais\i::__('Quantidade de fases de prestações de conta'), //alterar
             'type' => 'select',
             //torna obrigatorio preencher campo
             'validations' => array(
-                'required' => \MapasCulturais\i::__('Indique a quantidade de fases'),
+               'required' => \MapasCulturais\i::__('Indique a quantidade de fases'),
             ),
             'id' => 'selectCountPcback',
             'options' =>
             [
-                2 => \MapasCulturais\i::__(2), //alterar
-                1 => 1,
-                2 => 2,
-                3 => 3,
-                4 => 4,
-                5 => 5,
+               2 => \MapasCulturais\i::__(2), //alterar
+               1 => 1,
+               2 => 2,
+               3 => 3,
+               4 => 4,
+               5 => 5,
             ],
-        ];
+         ];
 
-        $def_opp = new Definitions\Metadata('count_total_pc', $conf);
-        $app->registerMetadata($def_opp, 'MapasCulturais\Entities\Opportunity');
+      $def_opp = new Definitions\Metadata('count_total_pc', $conf);
+      $app->registerMetadata($def_opp, 'MapasCulturais\Entities\Opportunity');
 
-        $app->registerController('prestacaodecontas', Controllers\PrestacaoDeContasController::class);
-    }
+      $app->registerController('prestacaodecontas', Controllers\PrestacaoDeContasController::class);
+   }
 }
--- layouts/parts/widget-opportunity-phases2.php
+++ layouts/parts/widget-opportunity-phases2.php
@@ -10,29 +10,3 @@ use MapasCulturais\i;
 
 
 
-<div ng-controller="OpportunityPhasesController">
-    <edit-box 
-        id="id-da-caixa" 
-        position="right" 
-        title="Título da caixa" 
-        spinner-condition="data.processando"
-        cancel-label="Fechar" 
-        submit-label="Enviar"
-        on-open="" 
-        on-cancel="newPhaseEditBoxCancel"
-        on-submit="newPhaseEditBoxSubmit"
-        spinner-condition=data.spinner
-        close-on-cancel='true'>
-        <div>
-            <label>
-                <li class="evaluation-methods--item">
-                    <input type="radio" id="onlyAccountabilityPhase" name="accountability_phase" value="accountability" ng-change="data.step = 'accountability'" ng-model="newPhasePostData.evaluationMethod">
-                        <?php i::_e('Prestação de Contas'); ?>
-                    <p class="evaluation-methods--name">
-                        <?php i::_e('Assinale caso a oportunidade exija prestação de contas'); ?>
-                    </p>
-                </li>
-            </label>
-        </div>
-    </edit-box>
-</div>
